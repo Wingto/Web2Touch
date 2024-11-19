2ac5392d37ea9f26e16fceaacfb6ac9503a24d9a
Wingto <627341343@qq.com>
2024-11-20 00:51:39 +0800

    updated

---
+++ Web2Touch——v1.1.10/dep_install.cmd
@@ -0,0 +1,6 @@
+
+python -m pip install --user --upgrade pip 
+pip3 install -r "/Users/zhengyali/Downloads/Web2Touch-main/web2Touch0.1.1/requirements.txt" --target="/Users/zhengyali/Downloads/Web2Touch-main/web2Touch0.1.1/dep/python" -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+
+
+
